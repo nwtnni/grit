c686a5d5a083c6c708f3c8c2c796271318b6508a
Newton Ni <nwtnni@gmail.com>
2021-05-25 12:40:34 -0400

    Implement skeleton `init` command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+use std::path;
+
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+enum Command {
+    Init(Init),
+}
+
+/// Initialize a new git repository.
+#[derive(StructOpt)]
+struct Init {
+    /// Path to directory to initialize.
+    ///
+    /// Default to current working directory if not provided.
+    root: Option<path::PathBuf>,
+}
+
+fn main() -> anyhow::Result<()> {
+    env_logger::init();
+
+    match Command::from_args() {
+        Command::Init(Init { root }) => {
+            let root = match root {
+                None => env::current_dir()?,
+                Some(root) => {
+                    fs::create_dir_all(&root)?;
+                    root.canonicalize()?
+                }
+            };
+
+            let mut path = root.join(".git");
+
+            for directory in &["objects", "refs"] {
+                path.push(directory);
+                fs::create_dir_all(&path)?;
+                path.pop();
+            }
+
+            log::info!("Initialized empty git repository at `{}`", root.display());
+
+            Ok(())
+        }
+    }
 }
