0b3249a48f12dad3a74fe9542b64120917d91178
Newton Ni <nwtnni@gmail.com>
2021-05-29 00:52:12 -0400

    Track reference information in new module

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod database;
 mod lock;
 pub mod object;
+pub mod reference;
 mod util;
 pub mod workspace;
 
 pub use database::Database;
 pub use object::Object;
+pub use reference::Reference;
 pub use workspace::Workspace;
---
+++ src/reference.rs
@@ -0,0 +1,36 @@
+use std::fs;
+use std::io;
+use std::io::Write as _;
+use std::path;
+
+use crate::lock;
+use crate::object;
+
+#[derive(Clone, Debug)]
+pub struct Reference {
+    root: path::PathBuf,
+    head: path::PathBuf,
+}
+
+impl Reference {
+    pub fn new(git: path::PathBuf) -> Self {
+        Reference {
+            root: git.join("refs"),
+            head: git.join("HEAD"),
+        }
+    }
+
+    pub fn set_head(&self, id: &object::Id) -> io::Result<()> {
+        let mut head = lock::File::new(self.head.clone())?;
+        write!(&mut *head, "{}", id)?;
+        Ok(())
+    }
+
+    pub fn head(&self) -> io::Result<Option<object::Id>> {
+        match fs::read(&self.head).and_then(|bytes| object::Id::from_bytes(&bytes)) {
+            Ok(id) => Ok(Some(id)),
+            Err(error) if error.kind() == io::ErrorKind::NotFound => Ok(None),
+            Err(error) => Err(error),
+        }
+    }
+}
