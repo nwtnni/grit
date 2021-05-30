5d26c33565a606f43a3b225869ad9685bb280c4e
Newton Ni <nwtnni@gmail.com>
2021-05-30 13:00:10 -0400

    Match `git` directory entry sort order
    Sort directories by appending a traililng '/' to the file name, to
    emulate sorting file entries by their complete path.

--- src/workspace.rs
+++ src/workspace.rs
@@ -1,3 +1,5 @@
+use std::iter;
+use std::os::unix::ffi::OsStrExt as _;
 use std::path;
 
 use crate::util::Tap as _;
@@ -24,7 +26,21 @@ impl IntoIterator for &'_ Workspace {
         let git = self.root.join(".git");
         walkdir::WalkDir::new(&self.root)
             .contents_first(true)
-            .sort_by_file_name()
+            .sort_by(|prev, next| {
+                let prev_name = prev.file_name().as_bytes();
+                let next_name = next.file_name().as_bytes();
+
+                // In order to match `git`'s sort order, we can to add a trailing
+                // directory separator '/' to the end of directory names.
+                //
+                // This emulates sorting all files by their complete paths without
+                // requiring us to store them all in memory.
+                match (prev.file_type().is_dir(), next.file_type().is_dir()) {
+                    (true, true) | (false, false) => prev_name.cmp(&next_name),
+                    (true, false) => prev_name.iter().chain(iter::once(&b'/')).cmp(next_name),
+                    (false, true) => prev_name.iter().cmp(next_name.iter().chain(iter::once(&b'/'))),
+                }
+            })
             .into_iter()
             .filter(move |entry| match entry {
                 Ok(entry) => !entry.path().starts_with(&git),
