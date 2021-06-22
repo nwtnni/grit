ae3e6b277f42fa3868a79fa839782ad1232f208b
Newton Ni <nwtnni@gmail.com>
2021-06-22 13:02:14 -0400

    Simplify and optimize workspace traversal
    Now that we commit from the index, we can remove the sorting logic from
    workspace traversal. Removing the `.contents_first(true)` call also
    allows us to skip over entire directories when we implement more
    fine-grained ignoring behavior later.

--- src/workspace.rs
+++ src/workspace.rs
@@ -1,5 +1,3 @@
-use std::iter;
-use std::os::unix::ffi::OsStrExt as _;
 use std::path;
 
 #[derive(Debug)]
@@ -23,28 +21,7 @@ impl Workspace {
         let root = self.root.join(relative);
         let git = self.root.join(".git");
         walkdir::WalkDir::new(root)
-            .contents_first(true)
-            .sort_by(|prev, next| {
-                let prev_name = prev.file_name().as_bytes();
-                let next_name = next.file_name().as_bytes();
-
-                // In order to match `git`'s sort order, we can to add a trailing
-                // directory separator '/' to the end of directory names.
-                //
-                // This emulates sorting all files by their complete paths without
-                // requiring us to store them all in memory.
-                match (prev.file_type().is_dir(), next.file_type().is_dir()) {
-                    (true, true) | (false, false) => prev_name.cmp(&next_name),
-                    (true, false) => prev_name.iter().chain(iter::once(&b'/')).cmp(next_name),
-                    (false, true) => prev_name
-                        .iter()
-                        .cmp(next_name.iter().chain(iter::once(&b'/'))),
-                }
-            })
             .into_iter()
-            .filter(move |entry| match entry {
-                Ok(entry) => !entry.path().starts_with(&git),
-                Err(_) => true,
-            })
+            .filter_entry(move |entry| !entry.path().starts_with(&git))
     }
 }
